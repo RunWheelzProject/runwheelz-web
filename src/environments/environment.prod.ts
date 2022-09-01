185790d5d59126e5b1f48d463ee8fe0b462a7070
DESKTOP-1KQHO0H\HOME <runwheelzproject@gmail.com>
2022-09-01 15:11:11 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  config : {
+    // apiUrl: "http://20.62.72.49:8080" ,
+      apiUrl: "http://localhost:8081" ,
+    //apiUrl: "http://devmsfbe.cd893934fc6a4c1d9181.eastus2.aksapp.io" ,
+  }
 };
